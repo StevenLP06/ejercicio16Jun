9563d1c56eb39ae06b2c75f6890101f5f542b4b4
StevenLP06 <stlopezp@unal.edu.co>
2025-06-19 18:23:05 -0500

    feat. Se agrega el insertar

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -28,57 +28,33 @@ class StudentController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        //
+        $student = new Student;
+        $student->name = $request->nombre;
+        $student->document = $request->documento;
+        $student->email = $request->correo;
+        $student->save();
+        return redirect()->route('students.index');
+        // return view('welcome');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $student = Student::find($id);
